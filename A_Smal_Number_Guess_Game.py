20b7f23deafb896838b6d70c4cc803a1bf0fb4d2
liuyangye <liuyangye@hotmail.com>
2018-09-19 21:06:54 +0800

    Add a game

--- A_Smal_Number_Guess_Game.py
+++ A_Smal_Number_Guess_Game.py
@@ -8,10 +8,9 @@ while bingo == False:
     answer = input()
 
     if answer < num:
-
-        print 'too small!'
+        print '%d is too small!' % answer
     if answer > num:
-        print 'too big!'
+        print '%d is too big!' % answer
     if answer == num:
-        print 'BINGO!'
+        print 'Bingo, %d is the right answer!' % answer
         bingo = True

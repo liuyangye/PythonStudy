33e1b6c749a97b2ad3edc75abb5d76138ee1e7bb
liuyangye <liuyangye@hotmail.com>
2018-10-31 19:16:57 +0800

    Insecure_Password_Locker

---
+++ Insecure_Password_Locker.py
@@ -0,0 +1,18 @@
+
+import sys, pyperclip
+
+PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+             'blog': 'VmALKvQyKAxiVH5G8v01if1MLZF3sdt',
+             'luggage': '12345'}
+
+if len(sys.argv) < 2:
+    print 'Usage: python Insecure_Password_Locker.py [account] - copy accout password'
+    sys.exit()
+
+account = sys.argv[1]  # first command line arg is the account name
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print 'Passord for ' + account + ' copied to clipboard.'
+else:
+    print 'There is no account named ' + account
